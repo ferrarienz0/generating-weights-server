ab7496bbc576ae6f15575777bb5887ee3087f6ba
Enzo Ferrari <ferrarienzobf@gmail.com>
2020-01-12 15:52:38 -0300

    Fix cors error

--- server.py
+++ server.py
@@ -5,9 +5,12 @@ from os import getenv
 from resources.MLMap import MLMap
 from flask import Flask
 from flask_restful import Api
+from flask_cors import CORS
 from dotenv import load_dotenv
 
 __APP = Flask(__name__)
+__CORS = CORS(__APP)
+__APP.config['CORS_HEADERS'] = 'Content-Type'
 __API = Api(__APP)
 
 __API.add_resource(MLMap, '/')
